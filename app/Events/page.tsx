52863c2419472b200c46f8a9ebdbbe7474a475bb
Muskans89 <109502928+Muskans89@users.noreply.github.com>
2024-10-04 12:47:18 +0530

    Update page.tsx

--- app/Events/page.tsx
+++ app/Events/page.tsx
@@ -218,7 +218,7 @@ export default function OurHomes() {
               <>
                 <Section
                   title="Shubham & Sumegha"
-                  imgSrc="/images/Img/03A_SHUBHAM_SUMEGHA_HALDI-1716-raju.jpg"
+                  imgSrc="/images/Img/ss.jpg"
                   location=" Raffels Udaipur"
                   videoLink="https://www.youtube.com/embed/JRT0zSUNNNU?autoplay=1&loop=1&mute=1&playlist=JRT0zSUNNNU&controls=0&rel=0"
                 />
